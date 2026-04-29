c1748189710f58de6a74437c18c4977cd41e9bb8
Nicolas Renck <nicolas.renck@gmail.com>
2026-04-29 18:37:58 -0300

    host3

--- eventProj/urls.py
+++ eventProj/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/events/')),
     path('admin/', admin.site.urls),
     path('', include('eventApp.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
