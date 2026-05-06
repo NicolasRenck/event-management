a3bd050ce0ea2e50e69a708f8e836f5cf9d3ba71
Nicolas Renck <nicolas.renck@gmail.com>
2026-05-05 23:02:39 -0300

    fix cloudinary4

--- eventProj/settings.py
+++ eventProj/settings.py
@@ -106,7 +106,9 @@ LOGOUT_REDIRECT_URL = 'home'
 ##MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CLOUDINARY_STORAGE = {
-   'CLOUDINARY_URL': config('CLOUDINARY_URL')
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
